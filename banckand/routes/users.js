5c955b7809557308301fff0375dd87346a5c7710
digitalpancenter <radigitalindai@gmail.com>
2025-07-12 20:18:00 +0530

    Update users.js

--- banckand/routes/users.js
+++ banckand/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User'); // Adjust path as needed
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching users', error: err });
+  }
 });
 
 module.exports = router;
